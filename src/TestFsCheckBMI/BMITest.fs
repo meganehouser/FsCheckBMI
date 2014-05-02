30b77833531ac8ec020767ed00ca3f38e5e82aef
meganehouser <sleepy.st818@gmail.com>
2014-05-02 20:29:25 +0900

    change BMI compute way

--- src/TestFsCheckBMI/BMITest.fs
+++ src/TestFsCheckBMI/BMITest.fs
@@ -8,6 +8,12 @@ open FsCheckBMI
 [<TestFixture>]
 type ``BMI算出のテスト``()=
     [<Test>]
-    member this.``BMI値の正負は体重の正負と等しい``() =
-        BMI.Compute(0, 10) |> ignore
-        fsCheck (fun (h:int) (w:int) -> (w > 0) = (BMI.Compute(h, w) > 0.0))
+    member this.``BMI値は身長より小さい``() =
+        fsCheck (fun (h:int) (w:int) -> 
+            BMI.Compute(h, w) <= (float h)
+            )
+            //==> (h > 0))
+
+//    [<Test>]
+//    member this.``BMI値の正負は体重の正負と等しい``() =
+//        fsCheck (fun (h:int) (w:int) -> (w > 0) = (BMI.Compute(h, w) > 0.0))
